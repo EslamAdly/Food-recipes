5f4d6c9f272350f3066865e365c503923d361d86
Eslam adly <eslamadly666@gmail.com>
2024-08-08 15:11:04 +0300

    end random meal

--- app/src/main/java/com/example/ratatouille/UI/HomeFragment.kt
+++ app/src/main/java/com/example/ratatouille/UI/HomeFragment.kt
@@ -1,60 +1,58 @@
 package com.example.ratatouille.UI
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.example.ratatouille.R
+import com.example.ratatouille.data.Meal
+import com.example.ratatouille.databinding.FragmentHomeBinding
+import com.example.ratatouille.internetServices.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+private lateinit var binding:FragmentHomeBinding
+lateinit var randomMeal:Meal
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchRandomMeal()
+
+        binding.randomMealImg.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            intent.putExtra("mealId",randomMeal.idMeal)
+            startActivity(intent)
+        }
     }
+    private fun fetchRandomMeal(){
+        lifecycleScope.launch(Dispatchers.IO){
+            randomMeal= RetrofitInstance.retrofitService.getRandomMeal().meals[0]
+            withContext(Dispatchers.Main){
+                Glide.with(this@HomeFragment).load(randomMeal.strMealThumb).into(binding.randomMealImg)
+
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/ratatouille/internetServices/MealAPI.kt
+++ app/src/main/java/com/example/ratatouille/internetServices/MealAPI.kt
@@ -2,8 +2,11 @@ package com.example.ratatouille.internetServices
 
 import com.example.ratatouille.data.MealResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MealAPI {
     @GET("random.php")
     suspend fun getRandomMeal():MealResponse
+    @GET("lookup.php?i")
+    suspend fun getMaelById(@Query("i") mealId:String):MealResponse
 }
